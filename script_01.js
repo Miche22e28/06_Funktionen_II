26f88b47ee2c14a61448c957c3069cb14fff0df0
miche22e28 <mbergamasco11@googlemail.com>
2023-07-18 12:34:10 +0200

    Rechner

--- script_01.js
+++ script_01.js
@@ -11,8 +11,23 @@
 4. Ausgabe in Konsole : 
 */
 
+// module: multiplication a * b |  test:
+output(multiply(3,2));
+output(multiply(3,-2));
+output(multiply(3,0));
+function multiply(a,b) {
+    return a*b;
+}
 
 
+// module: subtraction a - b |  test:
+output(subtract(3,2));
+output(subtract(3,-2));
+output(subtract(3,0));
+output(subtract(0,3));
+function subtract(a,b) {
+	return a - b;
+}
 
 
 // module: output | test:
@@ -22,3 +37,13 @@ function output(outputData) {
 	console.log(outputData);
 }
   
+
+// module: addition a + b |  test:
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+// function add(a,b) {
+// 	let c = a + b;
+// 	return a + b;
+// }
+
